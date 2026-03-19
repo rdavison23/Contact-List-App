79ee5e623390e61e91736f140c101fbe350c7795
Your Name <your-email@example.com>
2026-03-19 14:19:11 -0700

    setting up server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import pool from './db.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('.api/works', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
