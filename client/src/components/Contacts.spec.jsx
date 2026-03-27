14bb03d192cb29efa34db0ed5b125a11df553ae8
Your Name <your-email@example.com>
2026-03-26 19:02:34 -0700

    placeholder test

---
+++ client/src/components/Contacts.spec.jsx
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
+import Contacts from './Contacts';
+
+test('Contacts List renders', async () => {
+  expect(true).toBe(true);
+});
